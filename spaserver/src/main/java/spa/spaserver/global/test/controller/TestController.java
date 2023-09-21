80c36e11537551fd90041c05320dc63769751af4
witwint <witwint1234@gmail.com>
2023-09-21 22:12:24 +0900

    cicd5

--- spaserver/src/main/java/spa/spaserver/global/test/controller/TestController.java
+++ spaserver/src/main/java/spa/spaserver/global/test/controller/TestController.java
@@ -45,7 +45,7 @@ public class TestController {
 
 	@GetMapping("/text")
 	public ResponseEntity<?> getText() {
-		return response.success("test CICD4","success",HttpStatus.OK);
+		return response.success("test CICD5","success",HttpStatus.OK);
 	}
 
 }
