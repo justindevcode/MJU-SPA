49bdbab9f6e95f6c84fb4b1c0b7aad61e74dd387
witwint <witwint1234@gmail.com>
2023-09-24 16:16:56 +0900

    맴버 도메인

---
+++ spaserver/src/main/java/spa/spaserver/global/member/domain/AuthProvider.java
@@ -0,0 +1,10 @@
+package spa.spaserver.global.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum AuthProvider {
+	GOOGLE,
+	KAKAO,
+	NAVER
+}
---
+++ spaserver/src/main/java/spa/spaserver/global/member/domain/Authority.java
@@ -0,0 +1,6 @@
+package spa.spaserver.global.member.domain;
+
+public enum Authority {
+	ROLE_USER,
+	ROLE_ADMIN
+}
